d022a1bd40f194e6f7c766a86172e750af868298
merkui <merkui@yahoo.com.ar>
2023-03-24 00:03:00 -0300

    Add files via upload

---
+++ ItemListContainer.jsx
@@ -0,0 +1,8 @@
+export const ItemListContainer = () => {
+  //la lógica va siempre antes del return
+  return (
+    <article><div>
+    <p>Estaremos cargando nuestros productos pronto.</p>
+    </div></article>
+  );
+};
